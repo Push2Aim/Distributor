077e5a558a3283bd43984939064dbe1947387fa3
Gott50 <xxGott50xx@gmail.com>
2017-04-03 15:31:51 +0200

    make DataBase connection dependent on NODE_ENV

--- server/bookshelf.js
+++ server/bookshelf.js
@@ -3,4 +3,6 @@ const
  bookshelf = require('bookshelf');
  knexfile = require('../knexfile');
 
-module.exports = bookshelf(knex(knexfile.development));
+let env = process.env.NODE_ENV?process.env.NODE_ENV:"development";
+console.log("load knexfile environment:",env)
+module.exports = bookshelf(knex(knexfile[env]));
