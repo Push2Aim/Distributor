7a4ade2ce9c25d82d0828672ceefcbb4683a90c1
Gott50 <xxGott50xx@gmail.com>
2017-05-15 21:39:29 +0200

    add send Profile on Action "PROFILE"

---
+++ migrations/20170428222013_profile.js
@@ -0,0 +1,23 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTableIfNotExists("profile", function (table) {
+        table.increments().primary();
+        table.biginteger("fb_id").notNullable().unique();
+        table.integer("workout_level").defaultTo(0);
+        table.biginteger("xp").defaultTo(0);
+        table.string("main_strength").defaultTo("started");
+        table.integer("days_being_on_fitness_journey").defaultTo(0);
+        table.boolean("subscribed").defaultTo(false);
+        table.string("user_goal").defaultTo("else");
+
+        table.decimal("xp_knowledge").defaultTo(0);
+        table.decimal("xp_drill").defaultTo(0);
+        table.decimal("xp_sharing").defaultTo(0);
+
+        table.timestamps();
+    }).then(t => console.log("Table profile created", t));
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable("profile")
+        .then(t => console.log("Table profile dropped", t));
+};
