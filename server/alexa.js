65b0f1b78c9562ed2dbf79d8574deb0512f9dfec
Gott50 <xxGott50xx@gmail.com>
2017-08-23 20:06:34 +0200

    log calls to Stubs

--- server/alexa.js
+++ server/alexa.js
@@ -6,15 +6,15 @@ module.exports = {
 };
 
 function sendMessages(senderID, messages, response, url, reject = sendTextMessage, resolve) {
-
+    console.log("sendMessages(%s)", arguments)
 }
 function sendSpeech(recipientId, messageText) {
-
+    console.log("sendSpeech(%s)", arguments)
 }
 
 function sendTextMessage(recipientId, messageText, callback, timeOut) {
-
+    console.log("sendTextMessage(%s)", arguments)
 }
 function sendGenericMessage(recipientId, message, callback, timeOut, response, url) {
-
+    console.log("sendGenericMessage(%s)", arguments)
 }
