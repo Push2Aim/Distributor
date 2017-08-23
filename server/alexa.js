82bae71f87be5a952c967d2ab098809f46ee819f
Gott50 <xxGott50xx@gmail.com>
2017-08-23 19:58:14 +0200

    add Stubs

--- server/alexa.js
+++ server/alexa.js
@@ -1,33 +1,20 @@
-var Alexa = require('alexa-sdk');
+module.exports = {
+    sendMessages: sendMessages,
+    sendSpeech: sendSpeech,
+    sendTextMessage: sendTextMessage,
+    sendGenericMessage: sendGenericMessage,
+};
 
-// Load environment variables from .env file
-if (process.env.NODE_ENV !== "production")
-    loadEnvironmentVariables();
-function loadEnvironmentVariables() {
-    let dotenv = require('dotenv');
-    dotenv.load();
+function sendMessages(senderID, messages, response, url, reject = sendTextMessage, resolve) {
+
+}
+function sendSpeech(recipientId, messageText) {
+
 }
 
-exports.handler = function(event, context, callback){
-    var alexa = Alexa.handler(event, context, callback);
-    alexa.appId = process.env.ALEXA_APPLICAITON_ID;
-    console.log("appId:",alexa.appId,process.env.ALEXA_APPLICAITON_ID);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
+function sendTextMessage(recipientId, messageText, callback, timeOut) {
 
-var handlers = {
-    'LaunchRequest': function () {
-        this.emit('HelloWorldIntent');
-    },
+}
+function sendGenericMessage(recipientId, message, callback, timeOut, response, url) {
 
-    'HelloWorldIntent': function () {
-        this.emit(':tell', 'Hello World!');
-    },
-
-    'Unhandled': function () {
-        console.log("UNHANDLED");
-        var message = 'Say yes to continue, or no to end the game.';
-        this.emit(':ask', message, message);
-    }
-};
+}
