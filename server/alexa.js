03661ed61c9d709c1485116041470e171605ef63
Gott50 <xxGott50xx@gmail.com>
2017-08-23 20:42:22 +0200

    fix print Stub Arguments

--- server/alexa.js
+++ server/alexa.js
@@ -6,15 +6,15 @@ module.exports = {
 };
 
 function sendMessages(senderID, messages, response, url, reject = sendTextMessage, resolve) {
-    console.log("sendMessages(%s)", arguments)
+    console.log("sendMessages:", ...arguments);
 }
 function sendSpeech(recipientId, messageText) {
-    console.log("sendSpeech(%s)", arguments)
+    console.log("sendSpeech:", ...arguments);
 }
 
 function sendTextMessage(recipientId, messageText, callback, timeOut) {
-    console.log("sendTextMessage(%s)", arguments)
+    console.log("sendTextMessage:", ...arguments);
 }
 function sendGenericMessage(recipientId, message, callback, timeOut, response, url) {
-    console.log("sendGenericMessage(%s)", arguments)
+    console.log("sendGenericMessage:", ...arguments);
 }
