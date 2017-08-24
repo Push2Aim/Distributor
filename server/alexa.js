8bd02d8df50ae69fe6983047d1121627482f34b0
Gott50 <xxGott50xx@gmail.com>
2017-08-24 14:00:03 +0200

    fix TypeError: ui.sendTypingOn is not a function
        2017-08-24T11:57:31.152523+00:00 app[web.1]:     at sendApiAiRequest (/app/app.js:558:8)
    
        2017-08-24T11:57:31.152525+00:00 app[web.1]:     at buildApiAiRequestOptions.then.options (/app/app.js:468:13)
    
        2017-08-24T11:57:31.152525+00:00 app[web.1]:     at process._tickCallback (internal/process/next_tick.js:103:7)

--- server/alexa.js
+++ server/alexa.js
@@ -1,5 +1,6 @@
 module.exports = {
     sendMessages: sendMessages,
+    sendTypingOn: sendTypingOn,
     sendSpeech: sendSpeech,
     sendTextMessage: sendTextMessage,
     sendGenericMessage: sendGenericMessage,
@@ -8,6 +9,11 @@ module.exports = {
 function sendMessages(senderID, messages, response, url, reject = sendTextMessage, resolve) {
     console.log("sendMessages:", ...arguments);
 }
+
+function sendTypingOn(recipientId) {
+    console.log("sendTypingOn:", ...arguments);
+}
+
 function sendSpeech(recipientId, messageText) {
     return {
         "version": "1.0",
@@ -24,6 +30,7 @@ function sendSpeech(recipientId, messageText) {
 function sendTextMessage(recipientId, messageText, callback, timeOut) {
     console.log("sendTextMessage:", ...arguments);
 }
+
 function sendGenericMessage(recipientId, message, callback, timeOut, response, url) {
     console.log("sendGenericMessage:", ...arguments);
 }
