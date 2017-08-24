539f052b91b6e622876bd2c3898bdc7113370934
Gott50 <xxGott50xx@gmail.com>
2017-08-24 15:14:20 +0200

    add callback to async

--- server/alexa.js
+++ server/alexa.js
@@ -10,21 +10,20 @@ const
     async = require('async');
 
 function sendMessages(senderID, messages, response, url, reject = sendTextMessage, resolve) {
-    resolve = resolve || sendTextMessage;
     async.eachOfSeries(messages, (message, index, callback) => {
-        let timeOut = index === messages.length - 1 ? -1 : 0;
         switch (message.type) {
             case 0:
-                return sendTextMessage(senderID, message.speech, callback, timeOut);
+                return sendTextMessage(senderID, message.speech, callback);
             default:
                 console.log("skipped:", JSON.stringify(message));
+                callback;
                 break;
         }
     }, error => {
         if (error)
             return reject(senderID, "Ups, something went wrong: \n" + error);
         else
-            return resolve(senderID, "This Action is not supported yet!");
+            return sendSpeech(senderID, "This Action is not supported yet!");
     });
 }
 
