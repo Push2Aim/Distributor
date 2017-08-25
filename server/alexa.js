d553d4cdd92be4ab0c8fdfbf2b1cc1fe6546de4f
Gott50 <xxGott50xx@gmail.com>
2017-08-25 18:12:29 +0200

    add Card if Message is Image or GenericMessage

--- server/alexa.js
+++ server/alexa.js
@@ -12,7 +12,7 @@ function sendMessages(senderID, messages, response, url, reject = sendTextMessag
     let speech = response.result.fulfillment.speech;
     if (speech.length > 0)
         return sendSpeech(senderID, speech);
-
+    let card;
     try {
         for (let i = 0; i < messages.length; i++) {
             let message = messages[i];
@@ -25,12 +25,16 @@ function sendMessages(senderID, messages, response, url, reject = sendTextMessag
                 case 2:
                     speech += quickReplyToSpeech(message);
                     break;
+                case 3:
+                case 1:
+                    card = sendGenericMessage(senderID, message, callback, timeOut, response, url);
+                    break;
                 default:
                     console.log("skipped:", JSON.stringify(message));
                     break;
             }
         }
-        return sendSpeech(senderID, speech || "This Action is not supported yet!");
+        return sendSpeech(senderID, speech || "This Action is not supported yet!", card);
     } catch (error) {
         return reject(senderID, "Ups, something went wrong: \n" + error);
     }
@@ -46,10 +50,10 @@ function sendTypingOn(recipientId) {
     console.log("sendTypingOn:", ...arguments);
 }
 
-function sendSpeech(recipientId, messageText) {
+function sendSpeech(recipientId, messageText, card) {
     console.log("sendSpeech:", ...arguments);
     messageText = messageText.split(" action: ")[0];
-    return {
+    let out = {
         "version": "1.0",
         "response": {
             "outputSpeech": {
@@ -58,7 +62,9 @@ function sendSpeech(recipientId, messageText) {
             },
             "shouldEndSession": false
         }
-    }
+    };
+    if (card) out.response.card = card;
+    return out;
 }
 
 function sendTextMessage(recipientId, messageText, callback, timeOut) {
@@ -67,4 +73,20 @@ function sendTextMessage(recipientId, messageText, callback, timeOut) {
 
 function sendGenericMessage(recipientId, message, callback, timeOut, response, url) {
     console.log("sendGenericMessage:", ...arguments);
+
+    let duration = response ? response.result.parameters.duration || 0 : 0;
+    let amount = duration ? duration.amount : 30;
+
+    let out = {
+        type: "Standard",
+        image: {
+            largeImageUrl: message.imageUrl || "https://jspicgen.herokuapp.com/?type=WYN&duration=" + amount,
+        }
+    };
+    if (message.title)
+        out.title = message.title.replace("$duration.amount", amount);
+    if (message.subtitle)
+        out.text = message.subtitle;
+
+    return out;
 }
